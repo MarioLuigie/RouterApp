4d91eedd53998bb7096f890b942232e13eb74fb8
Mariusz Łotocki <mk.lotocki@gmail.com>
2023-09-04 00:13:23 +0200

    Fix multiple video playback problems
    create a function that changes the paths of video files in Header.jsx
    resolve problems with video refresh optimization by React (key attribute)
    resolve problem with async setVideoIndex in Header.jsx

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,15 +1,48 @@
+import { useRef, useState } from "react"
 import PropTypes from 'prop-types'
-import boublesVideo from "../assets/videos/boubles.mp4"
+import danceVideo01 from "../assets/videos/dance01.mp4"
+import danceVideo02 from "../assets/videos/dance02.mp4"
 import '../styles/layouts/Header.scss'
 
 export default function Header ({
     title
 }) {
 
+    const loop = false
+    const [videoIndex, setVideoIndex] = useState(1)
+    const videoRef = useRef(null);
+    const videos = [danceVideo01, danceVideo02]
+
+    const handleChangeVideo = () => {
+        if (videoRef.current) {
+            setTimeout(() => {
+                videoRef.current.play()
+            }, 10)
+            setVideoIndex(prevVideoIndex => (
+                prevVideoIndex === videos.length - 1 
+                    ? 0 
+                    : prevVideoIndex + 1
+            ))
+        }
+        console.log(videoIndex)
+        console.log("video ended")
+    }
+
     return (
         <header className='header'>
-            <video autoPlay loop muted className="header__video">
-                <source src={boublesVideo} type="video/mp4"/>
+            <video 
+                key={videoIndex}
+                autoPlay 
+                loop={loop}
+                muted 
+                className="header__video"
+                ref={videoRef}
+                onEnded={handleChangeVideo}
+            >
+                <source 
+                    src={videos[videoIndex]} 
+                    type="video/mp4"
+                />
             </video>
             <h2 className="header__title">{title}</h2>
         </header>
@@ -19,3 +52,8 @@ export default function Header ({
 Header.propTypes = {
     title : PropTypes.string.isRequired,
 }
+
+
+
+
+
